1bc9bdb7302f69ac8fdf090a3b0eb6e0efbaa4dc
Camelia <camelia.hirean@gmail.com>
2026-07-18 22:35:01 +0300

    [skip cd] Update for deployment

--- scripts/deploy_idle.sh
+++ scripts/deploy_idle.sh
@@ -8,22 +8,30 @@ if [ -z "$IMAGE_TAG" ] || [ -z "$ENVIRONMENT" ]; then
   exit 1
 fi
 
-# GHCR login (same as your working pipeline)
+# GHCR login
 echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USERNAME" --password-stdin
 
-APP_DIR="/opt/$ENVIRONMENT"
 PORT=$( [ "$ENVIRONMENT" = "blue" ] && echo "8001" || echo "8002" )
 
 echo "Deploying image $IMAGE_TAG to $ENVIRONMENT on port $PORT"
 
-docker pull ghcr.io/cameliahirean/medical-app:$IMAGE_TAG
+# Pull image
+docker pull ghcr.io/cameliahirean/medical-app:$IMAGE_TAG || {
+  echo "Image pull failed!"
+  exit 1
+}
 
+# Stop old container
 docker stop app-$ENVIRONMENT || true
 docker rm app-$ENVIRONMENT || true
 
+# Run new container
 docker run -d \
   --name app-$ENVIRONMENT \
   -p $PORT:$PORT \
-  docker pull ghcr.io/cameliahirean/medical-app:$IMAGE_TAG
+  ghcr.io/cameliahirean/medical-app:$IMAGE_TAG || {
+    echo "Container failed to start!"
+    exit 1
+}
 
 echo "Deployment to $ENVIRONMENT completed"
